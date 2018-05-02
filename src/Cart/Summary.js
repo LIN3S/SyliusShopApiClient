78d42a6fdfc5d430ebfb0bef75363ec46e093081
gorkalaucirica <gorka.lauzirika@gmail.com>
2018-05-02 16:12:50 +0200

    Capture not found exception in cart summary

--- src/Cart/Summary.js
+++ src/Cart/Summary.js
@@ -3,10 +3,11 @@ import axios from 'axios';
 import Session, {CartDoesNotExist} from "../Session";
 
 export default config => () => {
-  return new Promise(resolve => {
+  return new Promise((resolve, reject) => {
     try {
       axios.get(`${config.baseUrl}/shop-api/carts/${Session.Cart.id()}`)
-        .then(response => resolve(response.data));
+        .then(response => resolve(response.data))
+        .catch(error => reject(error));
     } catch (exception) {
       if (exception instanceof CartDoesNotExist) {
         return resolve(null);
@@ -16,4 +17,3 @@ export default config => () => {
     }
   });
 };
-

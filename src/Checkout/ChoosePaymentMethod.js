da3b9d9b0671f81f515f4056f8cee8827a8e596b
JotaGutierrez <jeliasgut@gmail.com>
2018-05-28 14:14:48 +0200

    Add auth params

--- src/Checkout/ChoosePaymentMethod.js
+++ src/Checkout/ChoosePaymentMethod.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
+import merge from 'lodash.merge';
 
 import session from "../session";
-import {contentTypeJson} from '../requestConfig';
+import {authParams, contentTypeJson} from '../requestConfig';
 
 export default config => ({paymentId, methodCode}) => {
   return new Promise(resolve => {
-    const headers = {
-      ...contentTypeJson(config),
-    };
+    const headers = merge(
+      authParams(config),
+      contentTypeJson(config)
+    );
 
     axios.put(
       `${config.baseUrl}/shop-api/checkout/${session(config).Cart.id()}/payment/${paymentId}`,
